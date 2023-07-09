35033b03d1fb6a55c0132eec4d375f6fe6314427
DanielOliverThomson@gmail.com <DanielOliverThomson@gmail.com>
2023-07-08 23:37:07 -0600

    Cleanup

--- flask/db/connection.py
+++ flask/db/connection.py
@@ -1,21 +1,21 @@
 import sqlite3
 
-from flask import g
+import flask
 
 
 def get_connection():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
+    if 'db' not in flask.g:
+        flask.g.db = sqlite3.connect(
             'sqlite.db',
             detect_types=sqlite3.PARSE_DECLTYPES,
         )
-        g.db.row_factory = sqlite3.Row
+        flask.g.db.row_factory = sqlite3.Row
 
-    return g.db
+    return flask.g.db
 
 
 def close_db(e=None):
-    db = g.pop('db', None)
+    db = flask.g.pop('db', None)
 
     if db is not None:
         db.close()
--- flask/db/initialization.py
+++ flask/db/initialization.py
@@ -1,21 +1,18 @@
 import click
 import flask
-
-from flask.cli import with_appcontext
+import flask.cli
 
 from .connection import close_db, get_connection
 
 
 def init_db():
     connection = get_connection()
-
-    # This will probably need to be updated
     with flask.current_app.open_resource('db/schema.sql') as f:
         connection.executescript(f.read().decode('utf8'))
 
 
 @click.command('init-db')
-@with_appcontext
+@flask.cli.with_appcontext
 def init_db_command():
     init_db()
     click.echo('Initiatlized the database')
